558ed7434e968acc4b5a2ae76f375e29141e3149
Vincent <vincent.nieutin@yahoo.fr>
2019-09-20 15:52:25 +0200

    Display the hospitals from big query boi on the map as markers

--- Site/SearchTrial.php
+++ Site/SearchTrial.php
@@ -1,4 +1,3 @@
-
 <?php
 //set up connection to DB
 $servername = "silva.computing.dundee.ac.uk";
@@ -11,8 +10,9 @@ if(!$conn){
 	die("Connection failed: " . mysqli_connect_error());
 	}
 if($conn){
-echo "connected <br>";
+//echo "connected <br>";
 
+//Get user input from the search page
 $injury = $_POST["injury_input"];
 $location = $_POST["location_input"];
 $range = $_POST["range_input"];
@@ -23,19 +23,31 @@ $medicare = $_POST["medicare_input"];
 //hardcoded input, this will need to change so user inoput is taken
 $description = "other";
 //var storing the query
-$sql = ("SELECT * FROM 2019indteam2db.codes_info WHERE description LIKE '%".$injury."%';");
+/*$sql = "SELECT * FROM 2019indteam2db.codes_info WHERE description LIKE '%".$injury."%';";
 
 //$result stores the result of the query, you can convert this to use in javascript, see david for this
 $result = mysqli_query($conn,$sql);
 $results = [];
 while($row = mysqli_fetch_array($result))
 {
- //   echo $row["code"];
-    $results[] = $row;
+		$results[] = $row;
+}*/
 
+$sql_coord = "SELECT x.code,x.providerId,x.averageTotalPayments,providerName,latitude,longitude
+FROM 2019indteam2db.financial_info_2017 x
+inner join 2019indteam2db.hospital_info y
+ON x.providerId = y.providerId
+and x.code = 165
+and averageTotalPayments <12000
+order by averageTotalPayments asc
+";
+
+$result = mysqli_query($conn,$sql_coord);
+$results = [];
+while($row = mysqli_fetch_array($result))
+{
+		$results[] = $row;
 }
-echo $results[0][0];
-echo $results[0][1];
 //echo "<script type='text\javascript'> var locations = ".$jsarray."; <script>";
 //closes connection to the database
  //mysqli_close($conn);
@@ -44,7 +56,7 @@ echo $results[0][1];
 <html>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link href="Trial.css" rel="stylesheet" type="text/css">
-<script src="https://maps.googleapis.com/maps/api/js?key="
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAWOLJZDit5LJs6RhOe2fjY3hJUKnqJjvs"
             type="text/javascript"></script>
 
             <script type="text/javascript">
@@ -95,8 +107,8 @@ echo $results[0][1];
         {
             document.write("<div class='card'>");
                     document.write("<div class='card-body'>");
-                        document.write( "<h1>" + locations[i]["code"] + "</h1>");
-                        document.write(locations[i]["description"] + "<br>");
+                        document.write( "<h1>" + locations[i]["providerName"] + "</h1>");
+                        document.write("$" + locations[i]["averageTotalPayments"] + "<br>");
                         document.write("<br>");
 
                     document.write("</div>");
@@ -106,38 +118,36 @@ echo $results[0][1];
     </div>
 
 
-  <div id="map">
+		<div id="map">
 
-<script type="text/javascript">
+			<script type="text/javascript">
 
         var map = new google.maps.Map(document.getElementById('map'),
         {
-            zoom: 10,
+            zoom: 1,
             center: new google.maps.LatLng(-33.92, 151.25),
             mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
         var infowindow = new google.maps.InfoWindow();
 
-        var marker, i;
-
-        for (i = 0; i < locations.length; i++)
+        for (var i = 0; i < locations.length; i++)
         {
-            marker = new google.maps.Marker({
-                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i]["latitude"], locations[i]["longitude"]),
                 map: map,
                 label: "H",
             });
 
             google.maps.event.addListener(marker, 'click', (function (marker, i) {
                 return function () {
-                    infowindow.setContent("<p>" + locations[i][0] + "<br/>" + locations[i][4] + "<br/>" + "<a href=" + locations[i][5]+" > More Info</a > ");
+                    infowindow.setContent("<p>" + locations[i]["providerName"] + "<br><a href=" + locations[i][5]+" > More Info</a > ");
                     //alert(locations[i][4])
                     infowindow.open(map, marker);
                 }
             })(marker, i));
         }
-    </script>
+    	</script>
     </div>
 </body>
 </html>
