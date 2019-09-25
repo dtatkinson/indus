0ee867b77ad97687ecbdf01e737cc8c970638860
David <davidtatkinson0@gmail.com>
2019-09-25 17:48:16 +0100

    added feature

---
+++ Site/pagination.js
@@ -0,0 +1,70 @@
+var current_page = 1;
+var records_per_page = 10;
+
+
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+        display();
+    }
+    
+}
+
+function nextPage()
+{
+   
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+        display();
+    }
+   
+}
+    
+function changePage(page)
+{
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var searchres = document.getElementById("searchres");
+   // var page_span = document.getElementById("page");
+ 
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    searchres.innerHTML = '	<ul class="pagination"><li class="page-item"><a class="page-link" id="btn_prev" href="javascript:prevPage()">Previous</a></li><li class="page-item"><a class="page-link" id="btn_next" href="javascript:nextPage()">Next</a></li></ul>';
+
+    for (var i = (page) * records_per_page; i < (page * records_per_page); i++) {
+        searchres.innerHTML += "<div class='card'>"+"<div class='card-body'>"+ "<h3>" + actualLocation[i]["providerName"] + "</h3>"+"$" + actualLocation[i]["averageTotalPayments"] + "<br>"+"<br>"+"<a href='#' value='i.value' onclick='show("+i+")'>View</a>"+"</div>"+"</div>";
+
+    }
+    //page_span.innerHTML = page;
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+}
+function test()
+{
+    alert("test");
+}
+
+function numPages()
+{
+    return Math.ceil(actualLocation.length / records_per_page);
+}
+/*
+window.onload = function() {
+    changePage(1);
+};
+*/
