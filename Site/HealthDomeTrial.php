afb619b53720848285c53adb54009d9dd27caa69
quinnlicious <Alex_znder@live.com>
2019-09-25 10:51:07 +0100

    Small ui, top bar

--- Site/HealthDomeTrial.php
+++ Site/HealthDomeTrial.php
@@ -61,13 +61,35 @@ function findLocation()
 <body>
 
     <head>
-  <nav class="navbar navbar-light bg-light">
+  <nav class="navbar navbar-expand navbar-light bg-light">
 	<a class="navbar-brand" href="#">
 		<img src="Images/healthdomeman.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
 		HealthDome
-	</a>
+
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+ 		</button>
+  		<div class="collapse navbar-collapse" id="navbarNav">
+   		 <ul class="navbar-nav">
+    	  	<li class="nav-item active">
+     	   	<a class="nav-link" href="#"></a>
+     	 	</li>
+     	 	<li class="nav-item active">
+        	<a class="nav-link" href="#">Home</a>
+      		</li>
+	   		<li class="nav-item">
+       	 	<a class="nav-link" href="#">About Us</a>
+      		</li>
+      		<li class="nav-item">
+       	 	<a class="nav-link" href="#">Contact
+			</a>
+    	    </li>
+    	 </ul>
+  		</div>
+		</a>
 	</nav>
-	<head>
+
+</head>
 
 	<div class="searchbox">
 			<div class="card">
