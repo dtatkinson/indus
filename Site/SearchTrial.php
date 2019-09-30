62bd401bdbcf0e8ede5970c683b7734a1f14a246
Matt <mchale88.mm@googlemail.com>
2019-09-30 13:49:01 +0100

    somewhat working version

--- Site/SearchTrial.php
+++ Site/SearchTrial.php
@@ -12,7 +12,7 @@ $conn = mysqli_connect($servername, $username, $password);
 if(!$conn){
 	die("Connection failed: " . mysqli_connect_error());
 	}
-if($conn){
+
 //echo "connected <br>";
 
 //Get user input from the search page
@@ -42,13 +42,15 @@ if(!empty($_POST["range_input"])){
 	$range = $DEFAULT_RANGE;
 }
 
-if(!empty($_POST["price_input"]))
+if(!empty($_POST["price_input"])){
 	$price = $_POST["price_input"];
-else
+}else{
 	$price = $DEFAUT_PRICE;
+}
 
-if(!empty($_POST["medicare_input"]))
-    $medicare = $_POST["medicare_input"];
+if(!empty($_POST["medicare_input"])){
+	$medicare = $_POST["medicare_input"];
+}
 
 //query stuff
 //hardcoded input, this will need to change so user inoput is taken
@@ -59,21 +61,43 @@ $sql_code = "SELECT * FROM 2019indteam2db.codes_info WHERE description LIKE '%".
 //$result stores the result of the query, you can convert this to use in javascript, see david for this
 $result_code = mysqli_query($conn,$sql_code);
 
-$sql_coord = "SELECT x.code,x.providerId,x.averageTotalPayments,providerName,latitude,longitude
-FROM 2019indteam2db.financial_info_2017 x
-inner join 2019indteam2db.hospital_information y
-ON x.providerId = y.providerId
-and x.code = ".mysqli_fetch_array($result_code)["code"]."
-and averageTotalPayments <".$price."
-order by averageTotalPayments desc
-;
-";
+if(empty($statecode=$_POST["state_input"])){
+	$sql_coord = "SELECT x.code,x.providerId,x.averageTotalPayments,providerName,latitude,longitude
+	FROM 2019indteam2db.financial_info_2017 x
+	inner join 2019indteam2db.hospital_information y
+	ON x.providerId = y.providerId
+	and x.code = ".mysqli_fetch_array($result_code)["code"]."
+	and averageTotalPayments <".$price."
+	order by averageTotalPayments desc
+	;
+	";
+}elseif(!empty($statecode=$_POST["state_input"])){
+	
+	$statecode = $_POST["state_input"];
+	$sql_coord = "SELECT x.code,x.providerId,x.averageTotalPayments,providerName,latitude,longitude
+	FROM 2019indteam2db.financial_info_2017 x
+	inner join 2019indteam2db.hospital_information y
+	ON x.providerId = y.providerId
+	and x.code = ".mysqli_fetch_array($result_code)["code"]."
+	and averageTotalPayments <".$price."
+	and y.providerState ='".$statecode."'
+	order by averageTotalPayments desc
+	;
+	";
+	
+}else{
+	header('Location:healthdometrial.php');
+	exit;
+}
 
 $result_coord = mysqli_query($conn,$sql_coord);
+
 $results_coord = [];
+
 while($row = mysqli_fetch_array($result_coord))
 {
 		$results_coord[] = $row;
+		
 }
 //closes connection to the database
 mysqli_close($conn);
@@ -83,8 +107,8 @@ mysqli_close($conn);
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link href="Trial.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBff9onASc5AUwK1DbBVXxW4dDoc-3qm1o&libraries=geometry" type="text/javascript"></script>
-
+<script src="https://maps.googleapis.com/maps/api/js?key=&libraries=geometry" type="text/javascript"></script>
+<script src="rating.js"></script>
             <script type="text/javascript">
 							var markers = [];
 							var actualLocation=[];
@@ -92,10 +116,12 @@ mysqli_close($conn);
 							var counter = 0;
 					    var x;
 	            var locations = <?php echo(json_encode($results_coord));?>;
+				alert(locations[0]["providerName"]);
 							var sortAscend = false;
             </script>
 
 <script type="text/javascript" src="pagination.js"></script>
+<script type="text/javascript" src="rating.js"></script>
 
 <head>
   <nav class="navbar navbar-expand navbar-light bg-light">
@@ -131,29 +157,33 @@ mysqli_close($conn);
 <body>
 	<div class="resultmanager">
 		<div class="sorting-container">
-			<select class="sorting-list mr-sm-2" id="sort_select">
+			<select class="sorting-list mr-sm-2" id="sort_select" onchange="sortHospitals()">
 				<option value="averageTotalPayments">Price</option>
+				<option value="center_distance">Distance</option>
 				<option value="rating">Rating</option>
 			</select>
-			<button class = "btn btn-primary" type="button" name="sort_button" onclick="sortHospitals()">Sort</button>
+			<button class = "btn btn-primary" type="button" name="sort_button" onclick="sortHospitals()">Sort</button> &nbsp; &nbsp; 
 		</div>
-<div class="markers-container">
-			<select class="sorting-list mr-sm-2" id="num_of_entries">
-					<option value=10>10</option>
-					<option value=20>20</option>
-					<option value=50>50</option>
-					<option value=100>100</option>
+		<div class="markers-container">
+	  <select class="sorting-list mr-sm-2" id="num_of_entries" onchange="numOfEntries()">
+	    <option value=10>10</option>
+	    <option value=20>20</option>
+	    <option value=50>50</option>
+	    <option value=100>100</option>
 
-				</select>
-				<button class = "btn btn-primary" type="button" name="sort_button" onclick="numOfEntries()">Filter</button>
-			</div>
-		<div class="pagination-buttons-holder">
+	   </select>
+	   
+	   		<div class="pagination-buttons-holder">
 			<ul class="pagination">
 				<li class="page-item"><a class="page-link" id="btn_prev" style="visibility:hidden;" href="javascript:prevPage()">Previous</a></li>
 				<li class="page-item"><a class="page-link" id="btn_next" href="javascript:nextPage()">Next</a></li>
 			</ul>
 		</div>
+	   
+  	</div>
+
 		<script type="text/javascript">
+
 			function sortHospitals(){
 
 				var sortValue = document.getElementById("sort_select").value;
@@ -169,14 +199,14 @@ mysqli_close($conn);
 				display();
 			}
 
-			function numOfEntries()
-			{
-				var entries = document.getElementById("num_of_entries").value;
-				records_per_page = entries;
-				clearHospitals();
-				display();
-			}
-			
+				function numOfEntries()
+					{
+					var entries = document.getElementById("num_of_entries").value;
+					records_per_page = entries;
+					clearHospitals();
+					display();
+					}
+
 			function clearHospitals(){
 				document.getElementById("searchres").innerHTML = "";
 			}
@@ -224,15 +254,17 @@ mysqli_close($conn);
 					if(center_distance < <?php echo($range);?>)
 					{
 						actualLocation[counter] = locations[i];
+						actualLocation[counter]["center_distance"] = center_distance;
 						counter++;
 					}
 				}
-				actualLocation = actualLocation.sort(function(a,b){return(a["averageTotalPayments"]-b["averageTotalPayments"])})//Sorts ascending
+				actualLocation = actualLocation.sort(function(a,b){return(a["averageTotalPayments"]-b["averageTotalPayments"])});//Sorts ascending
+				addRatings();
 		</script>
 
 	<script>
-display();
 
+display();
 function display()
 {
 	j=0;
@@ -256,14 +288,14 @@ function display()
 					animation: google.maps.Animation.DROP,
 					map: map
 					});
-					searchres.innerHTML += "<div class='card' value='i.value' onclick='show("+j+")'>"+"<div  class='card-body result-cards'>"+"<form id='map_form' action='MoreDetails.php' method='post' target='_blank'>" +"<h3>" + actualLocation[a]["providerName"] + "</h3>"+"$" + actualLocation[a]["averageTotalPayments"] + "<br>"+"<br>"+"<input type='text' hidden id='hosId' name='hosIdInput' class='form-control' value="+actualLocation[a]["providerId"]+">"+"<input type='text' hidden id='code' class='form-control' name='codeInput' value="+actualLocation[a]['code']+">"+"<br>"+"<br>"+"<button>More Details</button>"+"</form>"+ "</div>"+"</div>";
 
+			searchres.innerHTML += "<div class='card' value='i.value' onclick='show("+j+")'>"+"<div  class='card-body result-cards'>"+"<form id='map_form' action='MoreDetails.php' method='post' target='_blank'>"+ "<h3>" + actualLocation[a]["providerName"] + "</h3>"+"Average price: $" + actualLocation[a]["averageTotalPayments"] + "<br>Distance: " + Math.round(actualLocation[a]["center_distance"]*0.00062371) + " miles<br>"+"Rating: "+actualLocation[a]["rating"].toFixed(1)+"/10<br>"+ "<br>"+"<br>"+"<input type='text' hidden id='hosId' name='hosIdInput' class='form-control' value="+actualLocation[a]["providerId"]+">"+"<input type='text' hidden id='hosId' name='hosIdInput' class='form-control' value="+actualLocation[a]["providerId"]+">"+"<input type='text' hidden id='code' class='form-control' name='codeInput' value="+actualLocation[a]['code']+">"+"<br>"+"<br>"+"<button>More Details</button>"+"</form>"+ "</div>"+"</div>";
 			j++;
 			google.maps.event.addListener(marker, 'click', (function (marker, a)
 			{
 				return function ()
 				{
-					map.panTo(marker.getPosition());
+					map.setCenter(marker.getPosition());
 					map.setZoom(10);
 					infowindow.setContent("<h6>" + actualLocation[a]["providerName"] + "</h6>"+"<br>");
 					infowindow.open(map, marker);
@@ -277,6 +309,7 @@ function display()
 	  {
             google.maps.event.trigger(markers[id], 'click');
       }
+	  
     	</script>
     </div>
 </body>
