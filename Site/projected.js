fb51076fd8e0bba69735ef0061e1822cba92c715
Matt <mchale88.mm@googlemail.com>
2019-10-03 16:40:35 +0100

    added method for calculating insured graph

--- Site/projected.js
+++ Site/projected.js
@@ -1,4 +1,4 @@
-var projected, avg, projectedval;
+var projected, avg, projectedval, projectedvali;
 var pera, perb, perc, perd, pere, perf;
 function getPercent(yeara,yearb){
     var projected;
@@ -21,5 +21,21 @@ function calculate(){
    
     projectedval = g * ((100+avg)/100);
     projectedval = projectedval.toFixed(2);
+
+}
+function calculatei(){
+    
+    //pera = getPercent(a,b);
+    //perb = getPercent(b,c);
+    //perc = getPercent(c,d);
+    perd = getPercent(di,ei);
+    pere = getPercent(ei,fi);
+    perf = getPercent(fi,gi);
+
+    var sum = perd+pere+perf;
+    avg = sum/3;
+   
+    projectedvali = gi * ((100+avg)/100);
+    projectedvali = projectedvali.toFixed(2);
 
 }
